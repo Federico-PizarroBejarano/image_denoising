9d0450634a7ef9480ab5e829fc17d2b998d68112
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-11-21 19:16:30 -0500

    Adding an extra normalization step at the end and saving images

--- main.py
+++ main.py
@@ -10,39 +10,55 @@ from filters.quadratic_filter import quadratic_filter
 from filters.TV_filter import TV_filter
 from filters.non_local_means_filter import non_local_means_filter
 from filters.non_local_wnnm_filter import non_local_wnnm_filter
-from utilities.utils import read_image, add_noise, PSNR
+from utilities.utils import read_image, add_noise, PSNR, create_results_directory, normalize_image
 
 
-def main(plot=False):
+def main(plot=False, savefigs=False):
     '''Runs the various filters on the provided images with varying noise levels
        and saves the results.
 
     Args:
         plot (bool): Whether to plot the noisy and cleaned images.
+        savefigs (bool): Whether to save the generated images.
     '''
 
     np.random.seed(0)
+
     PSNR_results = {'quad': {}, 'TV': {}, 'nlm': {}, 'wnnm': {}}
     time_results = {'quad': {}, 'TV': {}, 'nlm': {}, 'wnnm': {}}
 
-    for im_name in ['clock', 'boat', 'aerial', 'bridge', 'couple']:
+    images = ['clock', 'boat', 'aerial', 'bridge', 'couple']
+    variances = [0.01, 0.025, 0.05]
+
+    create_results_directory(images, variances)
+
+    for im_name in images:
         for key in PSNR_results.keys():
             PSNR_results[key][im_name] = []
             time_results[key][im_name] = []
 
-        for var in [0.01, 0.025, 0.05]:
+        for var in variances:
+            str_var = str(var).replace('.', '_')
+
             im = read_image(im_name)
             noisy_im = add_noise(im, var)
             start_time = time.time()
 
             quad_im = quadratic_filter(noisy_im, 0.75)
             quad_time = time.time()
+            quad_im = normalize_image(quad_im)
+
             TV_im = TV_filter(noisy_im, 0.5)
             TV_time = time.time()
+            TV_im = normalize_image(TV_im)
+
             nlm_im = non_local_means_filter(noisy_im, 7, 10, 0.1)
             nlm_time = time.time()
+            nlm_im = normalize_image(nlm_im)
+
             wnnm_im = non_local_wnnm_filter(noisy_im, 7, 10, 0.1)
             wnnm_time = time.time()
+            wnnm_im = normalize_image(wnnm_im)
 
             PSNR_results['quad'][im_name].append(PSNR(original_im=im, cleaned_im=quad_im))
             PSNR_results['TV'][im_name].append(PSNR(original_im=im, cleaned_im=TV_im))
@@ -54,38 +70,46 @@ def main(plot=False):
             time_results['nlm'][im_name].append(nlm_time - TV_time)
             time_results['wnnm'][im_name].append(wnnm_time - nlm_time)
 
-            if plot is True:
+            if plot is True or savefigs is True:
                 _, ax_original = plt.subplots()
                 ax_original.imshow(im, cmap='gray')
                 ax_original.set_title('Original Image')
 
-                _, ax_noisy = plt.subplots()
+                fig_noisy, ax_noisy = plt.subplots()
                 ax_noisy.imshow(noisy_im, cmap='gray')
-                ax_noisy.set_title('Noisy Image')
+                ax_noisy.set_title(f'Noisy Image, PSNR={round(PSNR(original_im=im, cleaned_im=noisy_im), 2)}')
 
-                _, ax_quad = plt.subplots()
+                fig_quad, ax_quad = plt.subplots()
                 ax_quad.imshow(quad_im, cmap='gray')
-                ax_quad.set_title('Quadratic Image')
+                ax_quad.set_title(f'Quadratic Image, PSNR={round(PSNR(original_im=im, cleaned_im=quad_im), 2)}')
 
-                _, ax_tv = plt.subplots()
+                fig_tv, ax_tv = plt.subplots()
                 ax_tv.imshow(TV_im, cmap='gray')
-                ax_tv.set_title('TV Image')
+                ax_tv.set_title(f'TV Image, PSNR={round(PSNR(original_im=im, cleaned_im=TV_im), 2)}')
 
-                _, ax_nlm = plt.subplots()
+                fig_nlm, ax_nlm = plt.subplots()
                 ax_nlm.imshow(nlm_im, cmap='gray')
-                ax_nlm.set_title('Non-local means Image')
+                ax_nlm.set_title(f'Non-local means Image, PSNR={round(PSNR(original_im=im, cleaned_im=nlm_im), 2)}')
 
-                _, ax_wnnm = plt.subplots()
+                fig_wnnm, ax_wnnm = plt.subplots()
                 ax_wnnm.imshow(wnnm_im, cmap='gray')
-                ax_wnnm.set_title('Weighted Nuclear Norm Minimization Image')
+                ax_wnnm.set_title(f'Weighted Nuclear Norm Minimization Image, PSNR={round(PSNR(original_im=im, cleaned_im=wnnm_im), 2)}')
 
-                plt.show()
+                if savefigs is True:
+                    fig_noisy.savefig(f'./results/{im_name}/var_{str_var}/noisy.png')
+                    fig_quad.savefig(f'./results/{im_name}/var_{str_var}/quad.png')
+                    fig_tv.savefig(f'./results/{im_name}/var_{str_var}/tv.png')
+                    fig_nlm.savefig(f'./results/{im_name}/var_{str_var}/nlm.png')
+                    fig_wnnm.savefig(f'./results/{im_name}/var_{str_var}/wnnm.png')
+
+                if plot is True:
+                    plt.show()
 
     print(PSNR_results)
     print(time_results)
-    with open('PSNR_results.pkl', 'wb') as f:
+    with open('./results/PSNR_results.pkl', 'wb') as f:
         pickle.dump(PSNR_results, f)
-    with open('time_results.pkl', 'wb') as f:
+    with open('./results/time_results.pkl', 'wb') as f:
         pickle.dump(time_results, f)
 
 
--- utilities/utils.py
+++ utilities/utils.py
@@ -1,5 +1,6 @@
 '''A collection of utility functions. '''
 
+import os
 from math import log10, sqrt
 
 import numpy as np
@@ -13,14 +14,13 @@ def read_image(filename):
         filename (str): The name of the image to be read (e.g. clock).
 
     Returns:
-        normalized_im (np.ndarray): The normalized, resized image.
+        im (np.ndarray): The normalized, resized image.
     '''
     im = Image.open(f'./images/{filename}.png')
     im = im.resize((256, 256))
     im = np.array(im)
-    max_pixel = 255.0
-    normalized_im = im / max_pixel
-    return normalized_im
+    im = normalize_image(im)
+    return im
 
 
 def add_noise(im, var=0.01):
@@ -39,6 +39,19 @@ def add_noise(im, var=0.01):
     noisy_im = np.clip(noisy_im, 0.0, 1.0)
     return noisy_im
 
+def normalize_image(im):
+    '''Normalizes an image to have pixels only from 0-1
+
+    Args:
+        im (np.ndarray): The image to be normalized.
+
+    Returns:
+        normalized_im (np.ndarray): The normalized image.
+    '''
+    max_pixel = np.max(im)
+    min_pixel = np.min(im)
+    normalized_im = (im - min_pixel) / max_pixel
+    return normalized_im
 
 def PSNR(original_im, cleaned_im):
     '''Calculates the Peak Signal-to-Noise Ratio (PSNR) between two images.
@@ -56,3 +69,10 @@ def PSNR(original_im, cleaned_im):
     max_pixel = 1.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
+
+def create_results_directory(images, variances):
+    '''Creates the necessary results directory structure. '''
+    for image in images:
+        for var in variances:
+            str_var = str(var).replace('.', '_')
+            os.makedirs(f'./results/{image}/var_{str_var}/', exist_ok=True)
