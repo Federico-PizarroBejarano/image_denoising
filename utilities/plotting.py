271c330969cd1497834cf5fb752596b99df08e97
Federico-PizarroBejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-11-30 16:31:47 -0500

    Cleaning up plotting

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -10,30 +10,6 @@ import matplotlib.pyplot as plt
 ordered_algos = ['quad', 'TV', 'nlm', 'wnnm']
 algo_colors = {'quad':'cornflowerblue', 'TV':'goldenrod', 'nlm':'pink', 'wnnm':'tomato'}
 
-def plot_graph(data, data_name, unit=''):
-    '''Plots a graph of the given data.
-
-    Args:
-        data (dict): The dictionary containing the data.
-        data_name (str): The name of the data being plotted.
-        unit (str): The unit of the data (if any).
-    '''
-
-    mean_results = {'quad':0, 'TV':0, 'nlm':0, 'wnnm':0}
-    labels = ['Quadratic', 'Total Variation', 'Non-Local Means', 'Weighted NNM']
-
-    for algo in mean_results.keys():
-        for image in data[algo].keys():
-            mean_results[algo] += sum(data[algo][image])/15
-
-    _, ax = plt.subplots()
-    bars = ax.bar(range(1, 5), mean_results.values(), color=['cornflowerblue', 'goldenrod', 'pink', 'tomato'])
-    ax.set_ylabel(f'{data_name} {unit}', fontsize=20)
-    ax.set_xticks(range(1, 5), labels=labels, fontsize=20)
-    ax.bar_label(bars, labels=np.round(list(mean_results.values()), 2), fontsize=15)
-    ax.set_title(f'Mean {data_name} for each Denoising Algorithm', fontsize=25)
-    plt.show()
-
 
 def plot_experiment(all_results, noise_type):
     '''Plots the results for every hyperparameter variation.
@@ -73,12 +49,15 @@ def plot_experiment(all_results, noise_type):
     bars = {}
 
     for idx, algo in enumerate(ordered_algos):
-        algo_name = algo.title()
+        if algo == 'quad':
+            algo_name = 'Quadratic'
+        else:
+            algo_name = algo.upper()
         position = ((num_bars-1)/2.0 - idx)*width
         bars[algo] = ax.bar(x - position, cert_data[algo], yerr=cert_data_std[algo], width=width, label=f'{algo_name}', color=algo_colors[algo], capsize=10)
 
     plt.xticks(range(3), labels, fontsize=25)
-    ax.legend(fontsize=25)
+    ax.legend(fontsize=25, loc='lower left')
     ax.set_ylabel('PSNR', fontsize=25)
 
     for algo in ordered_algos:
@@ -105,8 +84,5 @@ if __name__ == '__main__':
         PSNR_data = pickle.load(f)
     with open(f'./results/{noise_name}/time_results.pkl', 'rb') as f:
         time_data = pickle.load(f)
-
-    plot_graph(PSNR_data, 'PSNR', '')
-    plot_graph(time_data, 'Runtime', '(s)')
 
     plot_experiment(PSNR_data, noise_name)
